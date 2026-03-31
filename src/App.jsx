6418ff79e1a2c03dfa4dd36e4f07a8b034b6e849
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-03-31 08:50:51 +0600

    Add Cart component and integrate it into the App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Banner from "./component/Banner";
+import Cart from "./component/Cart";
 import Footer from "./component/Footer";
 import Models from "./component/Models";
 import NavBar from "./component/NavBar";
 
-const getModels = async () => { 
+const getModels = async () => {
   const res = await fetch("/public/models.json")
-  return res.json() 
+  return res.json()
 }
 
 const modelPromise = getModels()
@@ -20,7 +21,18 @@ function App() {
     <>
       <NavBar />
       <Banner />
+      
+      {/* name of each tab group should be unique */}
+
+      <div className="tabs tabs-box justify-center bg-transparent">
+        <input type="radio" name="my_tabs_1" className="tab w-30 rounded-full" aria-label="Models" defaultChecked />
+        <input type="radio" name="my_tabs_1" className="tab w-30 rounded-full" aria-label="Cart"/>
+      
+      </div>
+
+
       <Models modelPromise={modelPromise} />
+      <Cart></Cart>
       <Footer />
     </>
   );
